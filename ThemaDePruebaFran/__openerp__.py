1e9ba5c159cc5c8d3a14d9469e9b05600bfc9ee4
Fran <fabrilalvarez@danielcastelao.org>
2016-01-29 13:43:35 +0100

    probando

---
+++ ThemaDePruebaFran/__openerp__.py
@@ -0,0 +1,11 @@
+{
+  'name':'FRAN Tutorial theme',
+  'description': 'Esta es mi descripcion.',
+  'version':'1.0',
+  'author':'FranciscoJAbrilAlvarez',
+
+  'data': [ 'views/layout.xml' ],
+  'category': 'Theme/Creative',
+  'depends': ['website', 'website_less'],
+
+}
